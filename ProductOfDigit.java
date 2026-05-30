0ce1227d0fe6a7a2226e77baf87d8f4456d9d417
Adarsh2224-git <25114cn156@glbitm.ac.in>
2026-05-30 12:43:45 +0530

    Add files via upload

---
+++ ProductOfDigit.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class ProductOfDigit {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter the number");
+int num = sc.nextInt();
+int product = 1;
+while(num!=0){
+    int digit = num%10;
+    product = product*digit;
+    num = num /10;
+}
+System.out.println(product);
+    }
+
+    
+}
